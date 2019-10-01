fcd73f9f3263c1abda3a9899f239c266b29d361d
uninformed <iguaranteenoonehasthisusername@gmail.com>
2019-10-01 11:20:33 -0500

    Add error 405 handler for API

--- app/apiv1/errors.py
+++ app/apiv1/errors.py
@@ -7,5 +7,9 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @apiv1.errorhandler(400)
-def not_found(error):
+def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
+
+@apiv1.errorhandler(405)
+def method_not_allowed(error):
+    return make_response(jsonify({'error': 'Method not allowed'}), 405)
