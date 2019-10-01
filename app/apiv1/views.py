a07b1a7153788ebb09ec28042fe28a1b9da6641c
uninformed <iguaranteenoonehasthisusername@gmail.com>
2019-10-01 01:22:57 -0500

    Add update_item API method

--- app/apiv1/views.py
+++ app/apiv1/views.py
@@ -41,3 +41,33 @@ def delete_item(item_id):
     db.session.delete(item)
     db.session.commit()
     return jsonify({'result': True})
+
+@apiv1.route('/item/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    '''Update an existing item.'''
+    item = Item.query.filter_by(id=item_id).first()
+    if item is None:
+        abort(404)
+    if not request.json:
+        abort(400)
+    # launch a series of type checks
+    if 'title' in request.json and type(request.json['title']) != str:
+        abort(400)
+    if 'creator' in request.json and type(request.json['creator']) != str:
+        abort(400)
+    if 'publisher' in request.json and type(request.json['publisher']) != str:
+        abort(400)
+    if 'notes' in request.json and type(request.json['notes']) != str:
+        abort(400)
+    if 'location_id' in request.json and type(request.json['location_id']) != int:
+        abort(400)
+
+    # finally make the changes
+    item.title = request.json.get('title', item.title)
+    item.creator = request.json.get('creator', item.creator)
+    item.publisher = request.json.get('publisher', item.publisher)
+    item.notes = request.json.get('notes', item.notes)
+    item.locationid = request.json.get('location_id', item.location_id)
+    db.session.add(item)
+    db.session.commit()
+    return jsonify(item=item.serialize)
